cad74cd32dc0acae479122d3defb3bde2778744a
jymbcrc <96434559+jymbcrc@users.noreply.github.com>
2024-01-25 20:32:35 -0800

    Update library loading.py

--- Library loading/library loading.py
+++ Library loading/library loading.py
@@ -1,45 +1,51 @@
 from pyteomics import mgf
 
+class LibraryLoader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.spectra_library = None
 
-def library_loading(file_path):
-    '''loading different library format '''   
-    if file_path.endswith(".mgf"):
-        spectra_library = library_loading_mgf(file_path)
-    elif file_path.endswith(".msp"):
-        spectra_library = library_loading_msp(file_path)
-    else:
-        print("Library format not supported")
-    return(spectra_library)
+    def load_library(self):
+        if self.file_path.endswith(".mgf"):
+            self.spectra_library = self._load_mgf()
+        elif self.file_path.endswith(".msp"):
+            self.spectra_library = self._load_msp()
+        else:
+            print("Library format not supported")
+        return self.spectra_library
 
-def library_loading_mgf(file_path):
-    spectra_library = mgf.IndexedMGF(file_path, index_by_scans=True)
-    return spectra_library
-    
-def library_loading_msp(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        spectra = []
-        spectrum = {}  # Start with an empty dictionary
-        for line in file:
-            line = line.strip() # remove leading and trailing whitespace characters 
-            # Skip empty lines
-            if not line:
-                continue
-            # Check for new spectrum
-            if line.startswith('Name:'):
-                if 'mz' in spectrum:  # Check if the current spectrum already has 'mz' key
-                    spectra.append(spectrum)
-                    spectrum = {}  # Reset for new spectrum
+    def _load_mgf(self):
+        # Assuming mgf.IndexedMGF is a class from an external library
+        spectra_library = mgf.IndexedMGF(self.file_path, index_by_scans=True)
+        return spectra_library
 
-            if ':' in line:  # Meta data line
-                key, value = line.split(':', 1)
-                spectrum[key.strip()] = value.strip()
-            else:  # Spectrum data line
-                mz, intensity = line.split()
-                spectrum.setdefault('mz', []).append(float(mz))
-                spectrum.setdefault('intensity', []).append(float(intensity))
+    def _load_msp(self):
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            spectra = []
+            spectrum = {}  # Start with an empty dictionary
+            for line in file:
+                line = line.strip()  # remove leading and trailing whitespace characters
+                # Skip empty lines
+                if not line:
+                    continue
+                # Check for new spectrum
+                if line.startswith('Name:'):
+                    if 'mz' in spectrum:  # Check if the current spectrum already has 'mz' key
+                        spectra.append(spectrum)
+                        spectrum = {}  # Reset for new spectrum
 
-        # Add the last spectrum if it contains data
-        if 'mz' in spectrum:
-            spectra.append(spectrum)
+                if ':' in line:  # Meta data line
+                    key, value = line.split(':', 1)
+                    spectrum[key.strip()] = value.strip()
+                else:  # Spectrum data line
+                    mz, intensity = line.split()
+                    spectrum.setdefault('mz', []).append(float(mz))
+                    spectrum.setdefault('intensity', []).append(float(intensity))
 
-        return spectra
+            # Add the last spectrum if it contains data
+            if 'mz' in spectrum:
+                spectra.append(spectrum)
+
+            return spectra
+
+
